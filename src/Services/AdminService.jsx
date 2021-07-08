a69dff54e2921c5d51dd88e5e2c961c64850c52a
DHEENA-DAYALAN <77840635+DHEENA-DAYALAN@users.noreply.github.com>
2021-07-08 11:39:45 +0530

    Update AdminService.jsx

--- src/Services/AdminService.jsx
+++ src/Services/AdminService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const CONSUMER_API_BASE_URL = "http://ec2-18-234-178-162.compute-1.amazonaws.com:8081/cg/ebs";
+const CONSUMER_API_BASE_URL = "http://ec2-52-201-202-143.compute-1.amazonaws.com:8081/cg/ebs";
 
 
 
